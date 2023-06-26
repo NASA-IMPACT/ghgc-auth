ff40ad0bcf59fe45143c4d065c24b1d27b79aba0
amarouane-ABDELHAK <am0089@uah.edu>
2023-06-26 14:55:17 -0500

    update export values

--- infra/stack.py
+++ infra/stack.py
@@ -47,7 +47,7 @@ class AuthStack(Stack):
         CfnOutput(
             self,
             "userpool_id",
-            export_name="userpool-id",
+            export_name=f"{stack_name}-userpool-id",
             value=self.userpool.user_pool_id,
         )
 
